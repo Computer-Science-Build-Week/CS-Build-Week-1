d2a792a760f98284db1a77e2c8e878d083a05f28
Nabeelah <noblenab13@gmail.com>
2019-10-25 13:52:35 +0100

    login and registration fixes

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import include
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^rest-auth/', include('rest_auth.urls')),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
 ]
